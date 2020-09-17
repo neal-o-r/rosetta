adb567cd7aae173a2aa97ecd495ebe293d5981ec
neal-o-r <neal@n-o-r.xyz>
2020-09-17 16:01:00 +0100

    add clojure

---
+++ clojure/counties.clj
@@ -0,0 +1,44 @@
+(require '[clojure.string :as str])
+
+(defn readfile-linesplit [filename]
+  (list* (str/split-lines (slurp filename)))
+  )
+
+(def counties (readfile-linesplit "../data/counties.txt"))
+(def words (readfile-linesplit "../data/enable1.txt"))
+
+
+(defn longest [a b]
+  (if (> (count a) (count b)) a b)
+  )
+
+
+(defn share-letters? [a b] 
+  (boolean (some true? (map #(str/includes? b (str %)) a)))
+  )
+
+
+(defn share-counties? [word counties]
+  (map #(share-letters? % word) counties)
+  )
+
+
+(defn count-trues [bools]
+  ((frequencies bools) true) 
+  )
+
+
+(defn bool-filter [lst bools]
+  (for [x (map vector lst bools) :when (not (second x))] (first x))
+  )
+
+(defn count-share-counties [word counties]
+  (count-trues (share-states? word counties))
+  )
+
+
+(defn longest-mackerel [words counties]
+  (def mackerels (filter #(= (count-share-counties % counties) 49) words))
+  (print (reduce longest mackerels))
+  )
+
